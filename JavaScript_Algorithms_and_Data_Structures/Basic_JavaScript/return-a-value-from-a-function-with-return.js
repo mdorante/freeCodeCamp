561f064f2d072dc7086cd9f8675ba495a168ab35
mdorante <mdorante10@gmail.com>
2020-06-24 07:22:33 -0300

    Add Basic JS solutions (40 - 51)

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/return-a-value-from-a-function-with-return.js
@@ -0,0 +1,5 @@
+function timesFive(num) {
+  return num * 5;
+}
+
+var fifteen = timesFive(3);
---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/shopping-list.js
@@ -0,0 +1,7 @@
+var myList = [
+  ["Chocolate Bar", 15],
+  ["Lollipop", 30],
+  ["Chewing gum", 100],
+  ["Mint", 187],
+  ["Slim Jim", 98]
+  ];
