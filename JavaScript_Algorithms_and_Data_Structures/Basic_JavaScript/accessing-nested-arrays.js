f27c9664a1a1596885358e899cc4b2e6a22f1ea6
mdorante <mdorante10@gmail.com>
2020-06-25 21:28:01 -0300

    Add Basic JS solutions (83 - 93)

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/accessing-nested-arrays.js
@@ -0,0 +1,27 @@
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+/*
+Retrieve the second tree from the variable myPlants using object dot and array bracket notation.
+ */
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; 
