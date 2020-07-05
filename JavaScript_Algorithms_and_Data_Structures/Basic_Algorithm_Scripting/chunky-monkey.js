17e66eaaf626bef05448de027d09c706818981e6
mdorante <mdorante10@gmail.com>
2020-07-05 20:42:59 -0300

    Add last Algorithm solution (forgot it last time)

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_Algorithm_Scripting/chunky-monkey.js
@@ -0,0 +1,9 @@
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += size) { // generate indices we need in order to correctly slice arr, varies depending on size
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
+
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)); // [ [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6 ] ]
