552b3d3ec60187d33662cbce0c83d9432afe0475
mdorante <mdorante10@gmail.com>
2020-07-05 23:22:46 -0300

    Add OOP solutions (12 - 20)

---
+++ JavaScript_Algorithms_and_Data_Structures/Object_Oriented_Programming/remember-to-set-the-constructor-property-when-changing-the-prototype.js
@@ -0,0 +1,11 @@
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+Dog.prototype = {
+  constructor: Dog,
+  numLegs: 4,
+  eat: () => console.log("nom nom nom"),
+  describe: () => console.log("My name is " + this.name)
+};
